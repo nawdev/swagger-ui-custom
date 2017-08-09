d3d78192f816a0d0bcdaa297de4950cf0dfb8e8f
Heiko Hübscher <h.huebscher@heinlein-support.de>
2017-08-09 13:14:54 +0200

    intro babel transpiler

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 console.log('xxx')
+
+const x = () => { console.log(42) }
+x()
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
     ],
   },
   plugins: [
