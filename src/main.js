9325886e81550b5ea53d424677a6ee7497e2d422
Heiko Hübscher <h.huebscher@heinlein-support.de>
2017-08-09 15:51:49 +0200

    intro js yaml

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+import js2yml from 'js-yaml'
+
 import SwaggerUIBundle from 'swagger-ui-dist/swagger-ui-bundle.js'
 import SwaggerUIStandalonePreset from 'swagger-ui-dist/swagger-ui-standalone-preset.js'
 
 
-window.onload = function() {
+window.onload = function () {
+
+
 
   // Build a system
   const ui = SwaggerUIBundle({
     url: "/service.yaml",
+    // spec: xxx,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -21,3 +26,14 @@ window.onload = function() {
 
   window.ui = ui
 }
+
+
+const get_yaml = url => {
+  fetch(url)
+    .then(yaml => {
+      console.log(js2yml.load(yaml))
+  })
+}
+
+
+get_yaml('/service.yaml')
